b6a510604b127561e8d18d54d2065469c9dba8b5
Michael Mertl <Michael.Mertl@hs-augsburg.de>
2023-12-03 13:25:59 +0100

    MPA: remove filter buttons

--- mpa/obj/Debug/net7.0/mpa.AssemblyInfo.cs
+++ mpa/obj/Debug/net7.0/mpa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mpa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d961e49d020bcd57730a2fc9e4f6086e6a276ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef74665685f18bf0e62db495a40cc1a88ea6d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("mpa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mpa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
